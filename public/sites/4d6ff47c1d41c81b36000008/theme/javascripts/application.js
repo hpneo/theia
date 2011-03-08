4f4afe6f2cbb6012b8984e01c4da3dcaf1357657
HPNeo <hpneo@hotmail.com>
2011-03-08 04:14:35 -0800

    Actualizada condicion de engine para mostrar formulario de recursos

--- public/sites/4d6ff47c1d41c81b36000008/theme/javascripts/application.js
+++ public/sites/4d6ff47c1d41c81b36000008/theme/javascripts/application.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
 	
+	// Remove no-js from html tag
 	$('html').removeClass('no-js');
 	
+	/*
+	 * Background color change
+	 */
 	$("a.anchorLink").click(function(){
+		
+		if ($('body').is(':animated')) {
+			return false;
+		}
+		
+		if ( $(this).hasClass('active') == true ) {
+			return false;
+		}
 		
 		//var colors = new Object();
 		var colors = new Array();
@@ -16,12 +28,35 @@ $(document).ready(function(){
 		return false;
 	});	
 	
+	/*
+	 * Scroll to
+	 */
 	$("a.anchorLink").anchorAnimate({speed:1100});
 	
+	/*
+	 * Banner's slider
+	 */
 	$('#section-brand .banner-content').cycle({
 		timeout:4000,
 		speed:500,
 		sync: 1
+	});
+	
+	
+	$(window).bind("scroll", function(event) {
+		 
+		 $(".section:in-viewport").each(function(){
+			 menu_top = $("#menu").offset().top;
+			 section_top = $(this).offset().top;
+			 
+			 
+			 if (menu_top - 130 > section_top){
+				 var name = $(this).find('a.link-anchor').attr("name");
+				 //$("a.anchorLink").click();
+				 //console.log(name);
+			}			 
+		 });
+		 
 	});
 
 });
--- public/sites/4d6ff47c1d41c81b36000008/theme/javascripts/myhead.js
+++ public/sites/4d6ff47c1d41c81b36000008/theme/javascripts/myhead.js
@@ -1,5 +1,6 @@
-head.js('javascripts/libs/jquery-1.5.1.min.js');
-head.js('javascripts/mylibs/jquery.anchor.js');
-head.js('javascripts/mylibs/jquery.color-min.js');
-head.js('javascripts/mylibs/jquery.cycle.lite.1.0.min.js');
-head.js('javascripts/application.js');
+head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/libs/jquery-1.5.1.min.js');
+head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/mylibs/jquery.anchor.js');
+head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/mylibs/jquery.color-min.js');
+head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/mylibs/jquery.cycle.lite.1.0.min.js');
+head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/mylibs/jquery.viewport.mini.js');
+head.js('http://alpha-theia.heroku.com.s3.amazonaws.com/sites/4d7003605b30553c28000005/theme/javascripts/application.js');
