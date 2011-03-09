827f9b236be5c1026e3401dbe0ef2bdd08f6b47a
Alvaro Pereyra <alvaro@xendacentral.com>
2011-03-09 18:41:02 -0500

    Changes template

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,6 @@ CarrierWave.configure do |config|
     config.s3_access_key_id = ENV['S3_KEY_ID']
     config.s3_secret_access_key = ENV['S3_SECRET_KEY']
     config.s3_bucket = ENV['S3_BUCKET']
-    
     # config.s3_cname = 'ENV['S3_CNAME']
   end
 
