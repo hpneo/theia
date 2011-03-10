dd6d5e5b7e6f794caeaa77f0c5253b7377e33d73
Alvaro Pereyra <alvaro@xendacentral.com>
2011-03-09 19:00:51 -0500

    Moves to file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,10 +7,10 @@ CarrierWave.configure do |config|
     config.root = File.join(Rails.root, 'public')
 
   when :production
-    config.storage = :s3
-    config.s3_access_key_id = ENV['S3_KEY_ID']
-    config.s3_secret_access_key = ENV['S3_SECRET_KEY']
-    config.s3_bucket = ENV['S3_BUCKET']
+    config.storage = :file
+    # config.s3_access_key_id = ENV['S3_KEY_ID']
+    # config.s3_secret_access_key = ENV['S3_SECRET_KEY']
+    # config.s3_bucket = ENV['S3_BUCKET']
     # config.s3_cname = 'ENV['S3_CNAME']
   end
 
